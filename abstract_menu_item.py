8e116cb39d43ecb236511b98778c50acac76e7b1
Reza Banitaba <ahmadreza.banitaba@gmail.com>
2019-10-11 21:17:03 -0700

    added id

--- abstract_menu_item.py
+++ abstract_menu_item.py
@@ -3,15 +3,44 @@ import datetime
 class AbstractMenuItem:
     """ creates menu item """ 
     
-    def __init__(self, menu_item_name, menu_item_no, date_added, price, calories )
+    def __init__(self, id, menu_item_name, menu_item_no, date_added, price, calories):
+        self._id = id
+        self._manu_item_name = menu_item_name
+        self._manu_item_no = menu_item_no
+        self._date_added = date_added
+        self._price = price
+        self._calories = calories
 
-    self._manu_item_name = menu_item_name
-    self._manu_item_no = menu_item_no
-    self._date_added = date_added
-    self._price = price
-    self._calories = calories
+
+    def get_id(self):
+        """ returns menu item id """
+        return self._id
 
     def get_menu_item_name(self):
         """ returns menu item name """ 
         return self._manu_item_name
+
+    def get_menu_item_no(self):
+        """ returns menu  item no """
+        return self._manu_item_no
+
+    def get_date_added(self):
+        """ returns date added """
+        return self._date_added
+    
+    def menu_item_description(self):
+        raise NotImplementedError("abstract method")
+
+    def set_price(self, price):
+        """ sets menu item price """
+        self._price = price
+
+    def get_price(self):
+        """ returns menu item price """
+        return self._price
+
+    def get_type(self):
+        raise NotImplementedError("abstract method")    
+
+
   
--- drink.py
+++ drink.py
@@ -4,8 +4,8 @@ from abstract_menu_item import AbstractMenuItem
 class Drink(AbstractMenuItem):
     """ creates drink """
 
-    def __init__(self, menu_item_name, menu_item_no, date_added, price, calories, manufacturer, size, is_fizzy, is_hot):
-        super().__init__(menu_item_name, menu_item_no, date_added, price, calories)
+    def __init__(self, id, menu_item_name, menu_item_no, date_added, price, calories, manufacturer, size, is_fizzy, is_hot):
+        super().__init__(id, menu_item_name, menu_item_no, date_added, price, calories)
         self._manufacturer= manufacturer
         self._size= size
         self._is_fizzy= is_fizzy
