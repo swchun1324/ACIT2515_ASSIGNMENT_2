6f0ec5a223d268aba79f9ba44488d8c3d706aca2
Reza Banitaba <ahmadreza.banitaba@gmail.com>
2019-10-15 19:34:35 -0700

    update

--- menu_item_manager.py
+++ menu_item_manager.py
@@ -11,7 +11,19 @@ class MenuItemManager:
 
     def add_menu_item(self, menu_item):
         """adds a menu item to the menu list"""   
-        self._menu.append(menu_item)
+        if menu_item not in self._menu:
+            self._menu.append(menu_item)
+
+    def next_available_id(self, menu_item):
+        menu_item_id = id(menu_item)
+        menu_item.set_id(menu_item_id)
+        return menu_item_id
+
+
+
+    def remove_menu_item(self, menu_item):
+        if menu_item in self._menu:
+            self._menu.remove(menu_item)
 
     
     def get_by_id(self, id):
@@ -19,10 +31,10 @@ class MenuItemManager:
             if menu_item.get_id() == id:
                 return menu_item
     
-    def get_all_by_type(self):
+    def get_all_by_type(self, item_type):
         menu_list = []
         for menu_item in self._menu:
-            if menu_item.get_type() == type:
+            if menu_item.get_type() == item_type:
                 menu_list.append(menu_item.menu_item_description())
         return menu_list                
 
@@ -37,16 +49,11 @@ class MenuItemManager:
         return menu_list 
                 
                 
-    
 
-    def update(self, menu_item):
+    def update(self, id):
         pass
-    
 
-    def remove_menu_item_by_id(self, menu_item):
-        if menu_item in self._menu:
-            if menu_item.get_id() == id:
-                self._menu.remove(menu_item)
+
 
 
     def get_menu_item_stats(self):
@@ -55,6 +62,11 @@ class MenuItemManager:
         total_num_menu_items = int(0)
         num_foods = int(0)
         num_drinks = int(0)
+        avg_price_food= float(0)
+        avg_price_drink = float(0)
+        item_price= float(0)
+        food_price_list = []
+        drink_price_list = []
 
         for menu_item in self._menu:
             total_num_menu_items += 1
@@ -64,17 +76,20 @@ class MenuItemManager:
                 num_drinks += 1
 
         for menu_item in self._menu:
-            price_list = []
-            if menu_item.get_type == "food":
+
+            if menu_item.get_type() == "food":
                 item_price = menu_item.get_price()
-                price_list.append(item_price)
-                avg_price_food = float(sum(price_list)/len(price_list))
+                food_price_list.append(item_price)
+                avg_price_food = sum(food_price_list)/len(food_price_list)
+
+        
 
             elif menu_item.get_type == "drink":
                 item_price = menu_item.get_price()
-                price_list.append(item_price)
-                avg_price_drink = float(sum(price_list)/len(price_list))
-                
+                drink_price_list.append(item_price)
+                avg_price_food = sum(drink_price_list)/len(drink_price_list) 
+
+
         stats = MenuItemStats(total_num_menu_items,num_foods, num_drinks, avg_price_food, avg_price_drink)
 
         return stats
